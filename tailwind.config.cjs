80283c1a32edfbcd0918a6e9cf33c8ad9e094146
andrew-zachary <andrew_zachary@outlook.com>
2023-01-12 00:00:00 +0000

    shopping cart with alpinejs and tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		maxWidth: {
+			'xs': '32rem',
+			'sm': '48rem',
+			'md': '76.8rem',
+			'lg': '99.2rem'
+		},
 		extend: {},
 	},
 	plugins: [],
