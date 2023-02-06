bec9db3eb9ad3d0167ee994263fc3d0dfda8a247
andrew-zachary <andrew_zachary@outlook.com>
2023-02-06 13:19:26 +0200

    images lazy-loading

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,8 +1,11 @@
 import initCart from "./alpine-cart-page";
+import intersectionObserver from "./alpine-lazy-loading-page";
 
 const loader = (pageId) => {
     if(pageId === 'cart-page') {
         initCart();
+    } else if(pageId === 'lazy-loading-page') {
+        intersectionObserver();
     }
 };
 
