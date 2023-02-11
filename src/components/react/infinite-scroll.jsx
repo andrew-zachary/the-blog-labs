a0c14449d3a361731d3065f94cbc1aa858b39a3d
andrew-zachary <andrew_zachary@outlook.com>
2023-02-11 00:00:00 +0000

    add loader and state/hasMore

--- src/components/react/infinite-scroll.jsx
+++ src/components/react/infinite-scroll.jsx
@@ -3,31 +3,38 @@ import { useScroll, useAsyncFn, useThrottleFn } from "react-use";
 
 import ProductsList from './layouts/products-list';
 
-export default ({url}) => {
+export default ({url, target, toLimit, toSkip}) => {
     const scrollRef = useRef(null);
+    const [hasMore, setHasMore] = useState(true);
     const [skip, setSkip] = useState(0);
     const {y} = useScroll(scrollRef);
 
     const [response, doFetch] = useAsyncFn(async () => {
-        const result = await (await fetch(`${url}/?limit=5&skip=${skip}`)).json();
-        setSkip(value => value += 5);
+        const result = await (await fetch(`${url}/?limit=${toLimit}&skip=${skip}`)).json();
+        setSkip(value => value += parseInt(toSkip));
+
+        if(result[target].length < toLimit) setHasMore(false);
+
         return result;
     }, [skip]);
 
     const tryFetching = () => {
-        if(response.loading) return;
+        if(response.loading || !hasMore) return;
         doFetch();
     };
 
     const throttledBottomScroll = useThrottleFn((currentY) => {
         return Math.ceil(currentY + scrollRef.current.clientHeight) >= (scrollRef.current.scrollHeight - 150)?true:false;
-    }, 1000, [y]);
+    }, 400, [y]);
 
     useEffect(() => {
         if(throttledBottomScroll) tryFetching();
-    }, [throttledBottomScroll])
+    }, [throttledBottomScroll]);
 
     return <div ref={scrollRef} id="infinite-scroll-page" className="h-full overflow-y-auto">
         <ProductsList response={response.value} />
+        { hasMore && <div className="p-4 text-center">
+            <span className="loader"></span>
+        </div> }
     </div>
 };
