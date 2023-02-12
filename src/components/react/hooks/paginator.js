ed03464bc7e86c7807308144d2a7429ccbd08a33
andrew-zachary <andrew_zachary@outlook.com>
2023-02-12 13:05:48 +0200

    paginator hook

---
+++ src/components/react/hooks/paginator.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { useAsyncFn } from "react-use";
+
+export default (url, target, perPage) => {
+    const [hasMore, setHasMore] = useState(true);
+    const [skip, setSkip] = useState(0);
+
+    const [response, doFetch] = useAsyncFn(async () => {
+        const result = await (await fetch(`${url}/?limit=${perPage}&skip=${skip}`)).json();
+        setSkip(value => value += result[target].length);
+
+        if(result[target].length < parseInt(perPage)) setHasMore(false);
+
+        return result;
+    }, [skip]);
+
+    return [response, doFetch, hasMore];
+}
