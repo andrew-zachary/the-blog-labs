36b98f95efa5a94f845b0dea648ea9dd638aa252
andrew-zachary <andrew_zachary@outlook.com>
2023-02-20 00:00:00 +0000

    vue-fundamentals vue-quiz-app base

---
+++ src/components/vue/fundamentals/form.vue
@@ -0,0 +1,8 @@
+<script setup>
+    const {index} = defineProps(['index']);
+</script>
+<style lang="scss"></style>
+<template>
+    <h1 class="text-4xl">{{ index }}</h1>
+    <form></form>
+</template>
