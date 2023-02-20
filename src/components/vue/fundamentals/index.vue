460964d03dac206b0a3276f716590c5e42d927d2
andrew-zachary <andrew_zachary@outlook.com>
2023-02-20 22:48:44 +0200

    vue-fundamentals vue-quiz-app base

---
+++ src/components/vue/fundamentals/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref } from 'vue';
+
+    import Form from './form.vue';
+
+    const forms = ref([
+        {'id': 1},
+        {'id': 2}
+    ]);
+</script>
+<style lang="scss"></style>
+<template>
+    <Form v-for="form in forms" :index="form.id"></Form>
+</template>
