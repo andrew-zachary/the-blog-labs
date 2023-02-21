205f6881f071b0c6a733c8ef988884f68b601753
andrew-zachary <andrew_zachary@outlook.com>
2023-02-21 21:36:19 +0200

    multilingual-register-form with locales

--- src/js/vue-app.js
+++ src/js/vue-app.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 
 //includes
 import veevalidate from './vue-app-veevalidate-plugin.js';
 
+//locales
+import en from '../locales/en.json';
+import ar from '../locales/ar.json';
+
 import App from '../components/vue/multilingual-register-form/index.vue';
 
-createApp(App).use(veevalidate).mount('#app');
+createApp(App).use(createI18n({
+    legacy: false,
+    locale: 'en',
+    fallbackRoot: 'en',
+    messages: {en, ar}
+})).use(veevalidate).mount('#app');
