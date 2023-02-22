c81a05a9b8cb64d3e7c8b9bc88a68be0320b3421
andrew-zachary <andrew_zachary@outlook.com>
2023-02-22 16:16:23 +0200

    multilingual-register-form remove code-dup

--- src/js/vue-app.js
+++ src/js/vue-app.js
@@ -10,9 +10,12 @@ import ar from '../locales/ar.json';
 
 import App from '../components/vue/multilingual-register-form/index.vue';
 
-createApp(App).use(createI18n({
+//parts
+const i18n = createI18n({
     legacy: false,
     locale: 'en',
     fallbackRoot: 'en',
     messages: {en, ar}
-})).use(veevalidate).mount('#app');
+});
+
+createApp(App).use(i18n).use(veevalidate).mount('#app');
