befdececfa1a267844c8c2dddb8760e7352a22ed
andrew-zachary <andrew_zachary@outlook.com>
2023-02-28 00:00:00 +0000

    moving vuejs examples inside a new directory

---
+++ src/js/vue/transitions-and-animations.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue';
+
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+import App from '../../components/vue/transitions-and-animations/index.vue';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('Dialog', Dialog);
+app.component('Button', Button);
+
+app.mount('#app');
