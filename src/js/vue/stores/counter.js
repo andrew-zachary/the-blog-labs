924fb736361aa67f832e605d5ebf6cefe3c3984d
andrew-zachary <andrew_zachary@outlook.com>
2023-03-03 23:47:25 +0200

    vue auth-with-pinia use computed successStyleClasses

--- src/js/vue/stores/counter.js
+++ src/js/vue/stores/counter.js
@@ -1,10 +1,12 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore('counter', () => {
     const count = ref(0);
-    const increment = () => {
-        count.value += 1;
+    const successStyleClasses = computed(() => count.value > 1000?'bg-black text-white':'');
+    const update = (amount) => {
+        console.log(amount);
+        count.value += parseInt(amount);
     };
-    return { count, increment };
+    return { count, update, successStyleClasses };
 });
