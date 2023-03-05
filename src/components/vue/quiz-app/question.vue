d0adde3224efd5da410c115b794bb11005885fd0
andrew-zachary <andrew_zachary@outlook.com>
2023-03-05 00:00:00 +0000

    vue quiz-app store paginate/restartQuiz

--- src/components/vue/quiz-app/question.vue
+++ src/components/vue/quiz-app/question.vue
@@ -2,9 +2,13 @@
     const { content } = defineProps(['content']);
 </script>
 <template>
-    <h1>{{ content.body }}</h1>
-    <ul>
-        <li v-for="answer of content.answers">{{ answer.body }}</li>
-    </ul>
+    <div class="question-content capitalize mt-8">
+        <div class="content-box w-full max-w-sm mx-auto">
+            <h1 class="text-4xl font-mont font-bold">{{ content.body }}</h1>
+            <ul class="mt-8">
+                <li class="text-3xl font-ssp mt-2" v-for="answer of content.answers" :key="answer.id">{{ answer.body }}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 <style lang="scss"></style>
--- src/components/vue/quiz-app/timer.vue
+++ src/components/vue/quiz-app/timer.vue
@@ -10,28 +10,45 @@
     const timerInterval = ref(null);
     const questionDurationProgress = ref(0);
     const currentSecound = ref(0);
+    const timeOut = ref(false);
 
-    onMounted(() => {
+    const startTimer = () => {
         timerInterval.value = setInterval((x) => {
             currentSecound.value += 1;
             questionDurationProgress.value = (currentSecound.value / (duration-1)) * 100;
             if(currentSecound.value === duration) {
-                emit('count-finished');
-                return clearInterval(timerInterval.value);
+                timeOut.value = true;
+                restartTimer();
             }
         }, 1000);
+    }
+
+    const restartTimer = () => {
+        setTimeout(() => {
+            clearInterval(timerInterval.value);
+            questionDurationProgress.value = 0;
+            currentSecound.value = 0;
+            timeOut.value = false;
+            emit('count-finished');
+            startTimer();
+        }, 1000);
+    }
+
+    onMounted(() => {
+        startTimer();
     });
 </script>
 <style lang="scss"></style>
 <template>
     <div class="px-8">
-        <h1 class="font-mont font-light text-9xl text-white flex justify-center">
+        <h1 class="font-mont font-light text-8xl text-white flex justify-center">
             <div class="inline-block p-20 leading-none rounded-full bg-black relative">
                 <span class="absolute top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%]">{{ currentSecound }}</span>
             </div>
         </h1>
         <div class="mt-8">
-            <ProgressBar :value="questionDurationProgress" :showValue="false" />
+            <h1 class="font-mont font-bold text-4xl text-center capitalize" v-if="timeOut">time out !!!</h1>
+            <ProgressBar v-else :value="questionDurationProgress" :showValue="false" />
         </div>
     </div>
 </template>
