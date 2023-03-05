7a38eeee6986f751ae3871aee63904e04cdbf11e
andrew-zachary <andrew_zachary@outlook.com>
2023-03-05 18:05:08 +0200

    vue quiz-app add more questions

--- src/components/vue/quiz-app/index.vue
+++ src/components/vue/quiz-app/index.vue
@@ -18,10 +18,10 @@
 </script>
 <style lang="scss"></style>
 <template>
-    <div v-if="quizStore.quizFinished" class="capitalize flex flex-col items-center">
-        <h1 class="text-6xl font-mont font-bold">finished</h1>
+    <div v-if="quizStore.quizFinished" class="flex flex-col items-center">
+        <h1 class="text-6xl font-mont font-bold capitalize">finished</h1>
         <div class="mt-4">
-            <button class="text-4xl font-ssp font-bold bg-black text-white px-4 py-2" @click="quizStore.restartQuiz">restart quiz</button>
+            <button class="text-4xl font-ssp font-bold bg-black text-white capitalize px-4 py-2" @click="quizStore.restartQuiz">restart quiz</button>
         </div>
     </div>
     <div v-else>
--- src/components/vue/quiz-app/question.vue
+++ src/components/vue/quiz-app/question.vue
@@ -2,11 +2,13 @@
     const { content } = defineProps(['content']);
 </script>
 <template>
-    <div class="question-content capitalize mt-8">
+    <div class="question-content capitalize mt-8 overflow-hidden">
         <div class="content-box w-full max-w-sm mx-auto">
             <h1 class="text-4xl font-mont font-bold">{{ content.body }}</h1>
             <ul class="mt-8">
-                <li class="text-3xl font-ssp mt-2" v-for="answer of content.answers" :key="answer.id">{{ answer.body }}</li>
+                <li class="text-3xl font-ssp mt-4" v-for="answer of content.answers" :key="answer.id">
+                    <span class="p-2 cursor-pointer hover:bg-black hover:text-white">{{ answer.body }}</span>
+                </li>
             </ul>
         </div>
     </div>
