851344af0559c01e48353fc0945a9cf9b93549e0
andrew-zachary <andrew_zachary@outlook.com>
2023-06-02 00:00:00 +0000

    nav btns list in ScrollMagic page

--- src/js/magic-scroll.js
+++ src/js/magic-scroll.js
@@ -1,8 +1,28 @@
 const loader = () => {
 
-    console.log(Circ);
+    const navBtns = document.querySelectorAll('.nav-btn');
 
     const controller = new ScrollMagic.Controller();
+
+    controller.scrollTo(function (newPos) {
+        TweenMax.to(window, 0.5, {scrollTo: {y: newPos}});
+    });
+
+    navBtns.forEach((node) => {
+        
+        node.addEventListener('click', (e) => {
+
+            e.preventDefault();
+
+            navBtns.forEach((node) => node.classList.remove('current'));
+
+            e.target.classList.add('current');
+
+            controller.scrollTo(e.target.getAttribute('href'));
+ 
+        });
+
+    });
 
     const tween1 = TweenMax.fromTo("#animate1", 0.5, {backgroundColor: "green", top: "-150%"}, {backgroundColor: "green", top: "100%"});
     const scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: "100%"});
@@ -12,11 +32,11 @@ const loader = () => {
     const scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: "100%"});
     scene2.setTween(tween2).addTo(controller);
 
-    const tween3 = TweenMax.fromTo("#animate3", 0.5, {backgroundColor: "blue", top: "-96px"}, {backgroundColor: "yellow", top: "100%"});
+    const tween3 = TweenMax.fromTo("#animate3", 0.5, {backgroundColor: "blue", top: "-96px"}, {backgroundColor: "rgba(255, 255, 0, 0.761)", top: "100%"});
     const scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", duration: "100%"});
     scene3.setTween(tween3).addTo(controller);
 
-    const tween4 = TweenMax.fromTo("#animate4", 0.5, {backgroundColor: "yellow", top: "-96px"}, {backgroundColor: "red", top: "100%"});
+    const tween4 = TweenMax.fromTo("#animate4", 0.5, {backgroundColor: "rgba(255, 255, 0, 0.761)", top: "-96px"}, {backgroundColor: "red", top: "100%"});
     const scene4 = new ScrollMagic.Scene({triggerElement: "#trigger4", duration: "100%"});
     scene4.setTween(tween4).addTo(controller);
 
