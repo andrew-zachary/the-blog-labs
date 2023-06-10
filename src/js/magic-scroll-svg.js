16e6af9deac3cf5e282796c2520991faaa9067d9
andrew-zachary <andrew_zachary@outlook.com>
2023-06-10 00:00:00 +0000

    animate dates dots within magic-scroll-svg page

--- src/js/magic-scroll-svg.js
+++ src/js/magic-scroll-svg.js
@@ -1,4 +1,7 @@
 const buildSpace = () => {
+
+    const dot1 = document.querySelector('g#dot-1');
+    const dot1Tween = TweenMax.to(dot1, 0.3, {opacity: 1}).pause();
     
     const line = document.querySelector('path#line');
     const lineTotalLength = line.getTotalLength();
@@ -7,11 +10,18 @@ const buildSpace = () => {
 
     const controller = new ScrollMagic.Controller();
 
-    const tween = new TimelineMax()
-    .add(TweenMax.fromTo(line, 0.3, {strokeDashoffset: 5307.32568359375, ease: Linear.easeNone}, {strokeDashoffset: 0, ease: Linear.easeNone}));
+    const tween = TweenMax.fromTo(line, 0.3, {strokeDashoffset: 5307.32568359375, ease: Linear.easeNone}, {strokeDashoffset: 0, ease: Linear.easeNone});
 
-    const scene = new ScrollMagic
-    .Scene({triggerElement: "#trigger1", duration: 2700})
+    const scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 2700})
+    .on('progress', ({progress}) => {
+
+        if(progress >= 0.044444444444444446) {
+            dot1Tween.play();
+        } else if(progress <= 0.044444444444444446) {
+            dot1Tween.reverse();
+        }
+
+    })
     .setTween(tween);
 
     scene.addTo(controller);
