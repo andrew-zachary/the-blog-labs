920ec59e8989ec4496ed1d953d269361e83fa5ce
andrew-zachary <andrew_zachary@outlook.com>
2023-06-14 00:00:00 +0000

    complete the register-form in register-form-subform

--- src/components/vue/register-form-subform/index.vue
+++ src/components/vue/register-form-subform/index.vue
@@ -1,6 +1,69 @@
 <script setup>
-    const text = 'text';
+    import LangSwitch from './lang-switch.vue';
+
+    const validationSchema = {
+        'name': 'required',
+        'email': 'required|email',
+        'password': 'required',
+        'confirm-password': 'confirmed:@password' 
+    };
+
+    const register = (values) => {
+        console.log(values);
+    };
+
+    const prepForTrans = (str) => `forms.register.${str}`.toLowerCase();
+    
 </script>
 <template>
-    <h1>{{ text }}</h1>
+
+    <LangSwitch />
+
+    <vee-form :validation-schema="validationSchema" @submit="register">
+
+        <vee-field name="name" v-slot="{field, errors}">
+            <div class="flex flex-col my-6">
+                <label for="name" class="text-4xl font-mont font-bold capitalize my-4">{{ $t(prepForTrans("name.label")) }}</label>
+                <input type="text" class="w-full text-4xl p-4 border border-black rounded-full" v-bind="field" />
+                <div class="text-3xl mt-2" v-if="errors.length > 0">
+                    * {{ $t(prepForTrans(errors[0] || "")) }}
+                </div>
+            </div>
+        </vee-field>
+
+        <vee-field name="email" v-slot="{field, errors}">
+            <div class="flex flex-col my-6">
+                <label for="name" class="text-4xl font-mont font-bold capitalize my-4">{{ $t(prepForTrans("email.label")) }}</label>
+                <input type="text" class="w-full text-4xl p-4 border border-black rounded-full" v-bind="field" />
+                <div class="text-3xl mt-2" v-if="errors.length > 0">
+                    * {{ $t(prepForTrans(errors[0] || "")) }}
+                </div>
+            </div>
+        </vee-field>
+
+        <vee-field name="password" v-slot="{field, errors}">
+            <div class="flex flex-col my-6">
+                <label for="name" class="text-4xl font-mont font-bold capitalize my-4">{{ $t(prepForTrans("password.label")) }}</label>
+                <input type="password" class="w-full text-4xl p-4 border border-black rounded-full" v-bind="field" />
+                <div class="text-3xl mt-2" v-if="errors.length > 0">
+                    * {{ $t(prepForTrans(errors[0] || "")) }}
+                </div>
+            </div>
+        </vee-field>
+
+        <vee-field name="confirm-password" v-slot="{field, errors}" :validateOnInput="true">
+            <div class="flex flex-col my-6">
+                <label for="name" class="text-4xl font-mont font-bold capitalize my-4">{{ $t(prepForTrans("confirm-password.label")) }}</label>
+                <input type="password" class="w-full text-4xl p-4 border border-black rounded-full" v-bind="field" />
+                <div class="text-3xl mt-2" v-if="errors.length > 0">
+                    * {{ $t(prepForTrans(errors[0] || "")) }}
+                </div>
+            </div>
+        </vee-field>
+
+        <button class="text-6xl font-bold text-white uppercase w-full p-4 bg-blue-600 rounded-full mt-12" type="submit">
+            {{ $t(prepForTrans("btns.submit")) }}
+        </button>
+
+    </vee-form>
 </template>
--- src/js/vue/register-form-subfrom.js
+++ src/js/vue/register-form-subfrom.js
@@ -1,12 +1,23 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import PrimeVue from 'primevue/config';
 
 import ProgressBar from 'primevue/progressbar';
+import veevalidate from './plugins/veevalidate.js';
+
+//locales
+import en from '../../locales/en.json';
+import ar from '../../locales/ar.json';
 
 import App from '../../components/vue/register-form-subform/index.vue';
 
+const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    fallbackRoot: 'en',
+    messages: {en, ar}
+});
+
 const registerFormSubform = createApp(App);
-
 registerFormSubform.component('ProgressBar', ProgressBar);
-
-registerFormSubform.use(PrimeVue).mount('#app');
+registerFormSubform.use(i18n).use(PrimeVue).use(veevalidate).mount('#app');
