8ad508348c065cc6ecb00a7a3da8e52c359d2de1
andrew-zachary <andrew_zachary@outlook.com>
2023-06-19 00:00:00 +0000

    mouseenter/mouseleave funcs for animated-blocks

--- src/js/animated-blocks.js
+++ src/js/animated-blocks.js
@@ -1,5 +1,32 @@
 const animatedBlocks = () => {
-    console.log('animated-blocks');
+    
+    const abColBlocks = document.querySelectorAll('.ab-col-blocks');
+    const getBlockPosition = (target) => {
+        return target.classList.contains('ab-col-top') ? 'nextElementSibling' : 'previousElementSibling';
+    }
+
+    abColBlocks.forEach( block => {
+        block.addEventListener('mouseenter', (e) => {
+            let blockPosition = '';
+
+            blockPosition = getBlockPosition(e.target);
+
+            e.target.style.height = '600px';
+            e.target[blockPosition].style.height = 0;
+        });
+    });
+
+    abColBlocks.forEach( block => {
+        block.addEventListener('mouseleave', (e) => {
+            let blockPosition = '';
+
+            blockPosition = getBlockPosition(e.target);
+
+            e.target.style.height = '300px';
+            e.target[blockPosition].style.height = '300px';
+        });
+    });
+
 };
 
 export default animatedBlocks;
