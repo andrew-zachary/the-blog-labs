47935148864d7aa4903a3c9c14e510f255c9d738
andrew-zachary <andrew_zachary@outlook.com>
2023-06-20 00:00:00 +0000

    set .swiper-button-next/.swiper-button-prev responsive position within animated-blocks page

--- src/js/animated-blocks.js
+++ src/js/animated-blocks.js
@@ -1,70 +1,68 @@
-// import Swiper JS
 import Swiper, { Navigation } from 'swiper';
 
-// import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 
 const animatedBlocks = () => {
     
-    // init blocks ------------------------------------------------->
-    const abColBlocks = document.querySelectorAll('.ab-col-blocks');
-    const getBlockPosition = (target) => {
-        return target.classList.contains('ab-col-top') ? 'nextElementSibling' : 'previousElementSibling';
-    };
+  // init blocks ------------------------------------------------->
+  const abColBlocks = document.querySelectorAll('.ab-col-blocks');
+  const getBlockPosition = (target) => {
+    return target.classList.contains('ab-col-top') ? 'nextElementSibling' : 'previousElementSibling';
+  };
 
-    abColBlocks.forEach( block => {
-        block.addEventListener('mouseenter', (e) => {
-            let blockPosition = '';
+  abColBlocks.forEach( block => {
+    block.addEventListener('mouseenter', (e) => {
+      let blockPosition = '';
 
-            blockPosition = getBlockPosition(e.target);
+      blockPosition = getBlockPosition(e.target);
 
-            e.target.style.height = '600px';
-            e.target.querySelector('.car').style.opacity = 1;
-            e.target[blockPosition].style.height = 0;
-            e.target[blockPosition].querySelector('.logo').style.opacity = 0;
-        });
+      e.target.style.height = '600px';
+      e.target.querySelector('.car').style.opacity = 1;
+      e.target[blockPosition].style.height = 0;
+      e.target[blockPosition].querySelector('.logo').style.opacity = 0;
     });
+  });
 
-    abColBlocks.forEach( block => {
-        block.addEventListener('mouseleave', (e) => {
-            let blockPosition = '';
+  abColBlocks.forEach( block => {
+    block.addEventListener('mouseleave', (e) => {
+      let blockPosition = '';
 
-            blockPosition = getBlockPosition(e.target);
+      blockPosition = getBlockPosition(e.target);
 
-            e.target.style.height = '300px';
-            e.target.querySelector('.car').style.opacity = 0;
-            e.target[blockPosition].style.height = '300px';
-            e.target[blockPosition].querySelector('.logo').style.opacity = 1;
-        });
+      e.target.style.height = '300px';
+      e.target.querySelector('.car').style.opacity = 0;
+      e.target[blockPosition].style.height = '300px';
+      e.target[blockPosition].querySelector('.logo').style.opacity = 1;
     });
-    // init blocks ------------------------------------------------->
-    
-    // create swiper ----------------------------------------------->
-    const swiper = new Swiper('.swiper', {
-      modules: [Navigation],
-      slidesPerView: 1,
-      spaceBetween: 0,
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 0
-        },
-        992: {
-          slidesPerView: 3,
-          spaceBetween: 0
-        },
-        1200: {
-          slidesPerView: 4,
-          spaceBetween: 0
-        }
+  });
+  // init blocks ------------------------------------------------->
+  
+  // init swiper ----------------------------------------------->
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation],
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 0
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 0
+      },
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 0
       }
-    });
-    // create swiper ----------------------------------------------->
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  // init swiper ----------------------------------------------->
 
 };
 
