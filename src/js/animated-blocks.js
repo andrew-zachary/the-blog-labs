95e9b02d1958d6bf55991d7933bcfe32615563f1
andrew-zachary <andrew_zachary@outlook.com>
2023-06-20 00:00:00 +0000

    init swiper within animated-blocks page

--- src/js/animated-blocks.js
+++ src/js/animated-blocks.js
@@ -1,5 +1,13 @@
+// import Swiper JS
+import Swiper, { Navigation } from 'swiper';
+
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const animatedBlocks = () => {
     
+    // init blocks ------------------------------------------------->
     const abColBlocks = document.querySelectorAll('.ab-col-blocks');
     const getBlockPosition = (target) => {
         return target.classList.contains('ab-col-top') ? 'nextElementSibling' : 'previousElementSibling';
@@ -30,6 +38,33 @@ const animatedBlocks = () => {
             e.target[blockPosition].querySelector('.logo').style.opacity = 1;
         });
     });
+    // init blocks ------------------------------------------------->
+    
+    // create swiper ----------------------------------------------->
+    const swiper = new Swiper('.swiper', {
+      modules: [Navigation],
+      slidesPerView: 1,
+      spaceBetween: 0,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 0
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 0
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 0
+        }
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+    // create swiper ----------------------------------------------->
 
 };
 
