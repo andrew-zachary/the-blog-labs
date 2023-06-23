d901a9bca6a0a35681291bb9261b0d3d364cb6d2
andrew-zachary <andrew_zachary@outlook.com>
2023-06-23 00:00:00 +0000

    load new items within masonry page

--- src/js/masonry.js
+++ src/js/masonry.js
@@ -2,21 +2,57 @@ import * as Masonry from 'masonry-layout';
 
 const masonry = () => {
 
-    const list = document.querySelector('.list');
+    const items = [
+        {
+            desc:'11- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit officia placeat, tempora atque id reprehenderit?',
+        },
+        {
+            desc:'12- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem tenetur a deleniti, quaerat velit saepe non dignissimos quisquam magnam voluptates sunt porro possimus id praesentium?'
+        },
+        {
+            desc:'13- Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit, libero ipsum. Voluptatum!'
+        },
+        {
+            desc: '14- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias,'
+        },
+        {
+            desc: '15- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, neque placeat ea nostrum ad in, id ipsam rerum architecto asperiores maxime iure assumenda suscipit. Quos voluptatem atque dolores sed vel commodi cumque animi ipsum'
+        },
+        {
+            desc: '16- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel aut sint praesentium doloribus unde aperiam eligendi iste enim? Modi, ducimus!'
+        },
+        {
+            desc: '17- Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident, perspiciatis.'
+        },
+        {
+            desc: '18- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad nulla dolorum optio dolor?'
+        },
+        {
+            desc: '19- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem tenetur a deleniti, quaerat velit saepe non dignissimos quisquam magnam voluptates sunt porro possimus id praesentium.'
+        },
+        {
+            desc: '20- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, quisquam quia.'
+        }
+    ];
+
+    const list = document.querySelector('.main-list');
     const msry = new Masonry( list, {
-        itemSelector: '.list-item',
+        itemSelector: '.main-list_item',
         columnWidth: '.grid-sizer',
         gutter: 0,
         horizontalOrder: true,
     });
 
-    const node = document.createTextNode("11- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem tenetur a deleniti, quaerat velit saepe non dignissimos quisquam magnam voluptates sunt porro possimus id praesentium?");
-    const para = document.createElement("div");
-    para.classList.add('list-item','text-3xl', 'bg-yellow-500');
-    para.appendChild(node);
-
-    list.appendChild(para);
-    msry.appended(para);
+    items.forEach((item) => {
+        const node = document.createTextNode(item.desc);
+        const div = document.createElement("div");
+        div.classList.add('main-list_item','text-3xl', 'border', 'border-black');
+        div.appendChild(node);
+    
+        list.appendChild(div);
+        msry.appended(div);
+    });
+    
     msry.layout();
 };
 
